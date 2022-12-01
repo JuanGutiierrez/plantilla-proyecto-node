e00a65d4298c2b1eab1cb2e4de72dca2368b5ca4
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-01 15:22:21 -0600

    Se exportan metodos CRUM en el archivo productoController

---
+++ controllers/ProductoController.js
@@ -0,0 +1,28 @@
+import {  } from "module";
+
+const crearProducto = () => {
+    
+}
+
+const listarProductos = () =>{
+    
+}
+
+const listarProducto = () =>{
+    
+}
+
+const editarProducto = () =>{
+    
+}
+const elimiarProducto = () =>{
+    
+}
+
+export {
+    crearProducto,
+    listarProducto,
+    listarProductos,
+    editarProducto,
+    elimiarProducto
+}
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /* const express = require("express"); */
 import express from "express";
-import { dataBase } from './database/dataBase.js';
+import dataBase from './database/dataBase.js';
 
 const app = express()
 
+try {
+    await dataBase.authenticate()
+    console.log("Coneccion exitosa")
+} catch (error) {
+    console.log(error)
+}
+
 app.listen(3100, ()=>{
     console.log('Servidor corriendo en el puerto 3100')
+    console.log('http://localhost:3100')
 })
